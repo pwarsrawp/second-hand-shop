e044fd997c177fc812ec123434044511df20617a
pwarsrawp <pwarsrawp@gmail.com>
2023-08-10 00:17:49 +0200

    database fix, product detail styling, merge issues solved

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Spinner from "../components/Spinner";
 import { useParams, Link } from "react-router-dom";
