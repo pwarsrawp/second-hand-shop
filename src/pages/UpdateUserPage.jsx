9f754c0818a9f6c5fa9b18572bd91144d3644885
Shashika Samaradeera <thushanishashika23@gmail.com>
2023-08-09 19:46:13 +0200

    clean the logs

--- src/pages/UpdateUserPage.jsx
+++ src/pages/UpdateUserPage.jsx
@@ -7,21 +7,14 @@ const UpdateUserPage = () => {
   const navigate = useNavigate();
   const { userId } = useParams();
   const [currUser, setCurrUser] = useState(null);
-  // const [user, setUser] = useState();
   const { setUserUpdate, user } = useContext();
 
   useEffect(() => {
-    // fetchUser(userId, setUser);
     if (user) {
-      console.log("this is our curr user: ", user);
       setCurrUser(user);
     }
   }, []);
 
   return <>{user && <UserEditForm />}</>;
-
-    </>
-  );
-
 };
 export default UpdateUserPage;
